c1fa4475dae9dbf33f82d8c7175141214bf0f91d
Hoo <136200456@qq.com>
2018-11-08 16:50:25 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+      <van-tag type="success">标签</van-tag>
+  </div>
+</template>
+
+<script>
+import { Tag } from 'vant'
+export default {
+  components: {
+    [Tag.name]: Tag
+  }
+}
+</script>
