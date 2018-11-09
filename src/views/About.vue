33b6bbac5c5ca2aea4de51f347e924d9757ade72
Hoo <136200456@qq.com>
2018-11-09 11:54:02 +0800

    feat: page-skeleton-webpack-plugin插件生成骨架屏

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,14 +2,32 @@
   <div class="about">
     <h1>This is an about page</h1>
       <van-tag type="success">标签</van-tag>
+      <div style="height: 40px; border: 1px solid #ccc;"></div>
+      <van-circle
+              v-model="currentRate"
+              :rate="30"
+              :speed="100"
+              :text="text"
+      />
   </div>
 </template>
 
 <script>
-import { Tag } from 'vant'
+import { Tag, Circle } from 'vant'
 export default {
   components: {
-    [Tag.name]: Tag
+    [Tag.name]: Tag,
+    [Circle.name]: Circle
+  },
+  data () {
+    return {
+      currentRate: 0
+    }
+  },
+  computed: {
+    text () {
+      return this.currentRate.toFixed(0) + '%'
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,36 @@
+const path = require('path')
+const { SkeletonPlugin } = require('page-skeleton-webpack-plugin')
 module.exports = {
-  baseUrl: './'
+  baseUrl: './',
+  configureWebpack: {
+    plugins: [
+      new SkeletonPlugin({
+        pathname: path.resolve(__dirname, './src/shell'), // 用来存储 shell 文件的地址
+        staticDir: path.resolve(__dirname, './dist'), // 最好和 `output.path` 相同
+        routes: ['/', '/about'], // 将需要生成骨架屏的路由添加到数组中,
+        loading: 'chiaroscuro',
+        debug: true,
+        svg: {
+          color: '#EFEFEF',
+          shape: 'circle',
+          shapeOpposite: ['.Rating-gray_1kpffd5_0 svg']
+        },
+        image: {
+          shape: 'rect', // `rect` | `circle`
+          color: '#EFEFEF',
+          shapeOpposite: ['.mint-swipe-items-wrap img']
+        },
+        pseudo: {
+          color: '#EFEFEF', // or transparent
+          shape: 'circle', // circle | rect
+          shapeOpposite: ['.delivery-icon-hollow_3q8_B5r_0', '.index-premium_39rl0v9']
+        },
+        button: {
+          color: '#EFEFEF',
+          excludes: ['.mint-swipe-items-wrap a']
+        },
+        defer: 5000
+      })
+    ]
+  }
 }
